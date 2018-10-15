48bb61a9457612bdd74b867ed6aba62c5f51a593
Hunter Somers <42579246+hsomers@users.noreply.github.com>
2018-10-14 22:08:02 -0500

    Add files via upload

--- GooglePlayAnalysis.R
+++ GooglePlayAnalysis.R
@@ -1,32 +1,25 @@
 rm(list = ls())
 
 #Load packages
-suppressPackageStartupMessages(library(readxl))
 suppressPackageStartupMessages(library(ggplot2))
-suppressPackageStartupMessages(library(scales))
-suppressPackageStartupMessages(library(DBI))
 suppressPackageStartupMessages(library(dplyr))
-suppressPackageStartupMessages(library(reshape2))
-suppressPackageStartupMessages(library(ggmap))
-suppressPackageStartupMessages(library(choroplethr))
-suppressPackageStartupMessages(library(choroplethrMaps))
-suppressPackageStartupMessages(library(rvest))
-suppressPackageStartupMessages(library(RSQLite))
 
-#Import the csv files
+#-------------------------------------------------------------------
+#Analysis Below
+#-------------------------------------------------------------------
+
+#Import the csv file
 df <- read.csv('googleplaystore.csv', na.strings = c("NaN", "NA"))
-revs <- read.csv('googleplaystore_user_reviews.csv', na.strings = c("NaN", "NA"))
 
 #Clean out empty data
 df <- df[complete.cases(df), ]
-revs <- revs[complete.cases(revs), ]
 
 #What are the average scores for each app category?
 AvgSc <- select(df, Category, Rating) %>%
   group_by(Category) %>%
   summarize(AvgScore = mean(Rating)) 
 
-#What are the top 3 apps (in terms of downloads) in each category?
+#What are the top 3 apps (in terms of downloads) for each category?
 levels(df$Installs)
 df$Installs <- gsub('\\+','', df$Installs)
 df$Installs <- gsub(',','', df$Installs)
@@ -36,14 +29,19 @@ top_insts <- select(df, App, Category, Installs) %>%
   top_n(n = 3, wt = Installs) %>%
   arrange(Category, desc(Installs))
 
+#Make a plot of the top apps installed for each category
 qplot(Category, Installs, data = top_insts, geom = "point") +
   theme(axis.text.x = element_text(angle = 90)) + 
   scale_y_continuous(trans = "log10")
 
-#Which app type is the most popular? (most downloads)
-#How many games have > 1 genre label
+#Which app type is the most popular? (most total installs)
+print(select(df, Category, Installs) %>%
+  group_by(Category) %>%
+  summarise(TotalInst = sum(Installs)) %>%
+  top_n(n=1, wt = TotalInst))
+
 #Which app has the most downloads, and has the highest rating?
-#What is the ratio of free:paid apps?
-
-
-#What are some of the most used words for reviews of 3 or above
+temp <- select(df, App, Installs, Rating) %>%
+        distinct(App, Installs, Rating) %>%
+        top_n(n=1, wt = Installs) %>%
+        top_n(n=1, wt = Rating)
